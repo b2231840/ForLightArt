c5ad496da7719b0d09b2777cbe9514522abd6c77
b2231840 <b2231840@photon.chitose.ac.jp>
2025-04-29 11:57:04 +0900

    メインにコピーしただけ

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("話しかけてください...")
+    audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio, language="ja-JP")
+        print("認識結果:", text)
+    except sr.UnknownValueError:
+        print("認識できませんでした")
