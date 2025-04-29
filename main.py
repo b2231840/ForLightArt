5db7cad2b880b143c97833a64e51f9d88049ba29
b2231840 <b2231840@photon.chitose.ac.jp>
2025-04-29 12:01:42 +0900

    メインにコピーしただけ

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+def get_response(text):
+    if "こんにちは" in text:
+        return "こんにちは！今日はどうですか？"
+    elif "さようなら" in text:
+        return "またね〜！"
+    else:
+        return "ごめん、よくわからないや"
+
