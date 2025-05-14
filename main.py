d316c5c213edf46fe279e392dc579a341464d54a
b2231840 <b2231840@photon.chitose.ac.jp>
2025-05-14 18:10:09 +0900

    devのmain.pyを少し変更(まだエラー出る可能性あり)

--- main.py
+++ main.py
@@ -13,31 +13,9 @@ with sr.Microphone() as source:
         print("認識結果:", text)
     except sr.UnknownValueError:
         print("認識できませんでした")
+        system.exit(0)
 
 ##responce
-#def get_response(text):
-#    if "こんにちは" in text:
-#        return "こんにちは！今日はどうですか？"
-#    elif "さようなら" in text:
-#        return "またね〜！"
-#    else:
-#        return "ごめん、よくわからないや"
-
-
-##voice_output
-engine = pyttsx3.init()
-
-## 日本語音声があったらそれを選択
-for voice in engine.getProperty('voices'):
-	if "Japanese" in voice.name or "Haruka" in voice.name:
-		engine.setProperty('voice',voice.id)
-		break
-
-##engine.say("こんにちは、私はオフラインのAIです。")
-engine.say(get_response(text)) ##responseの関数で返答
-engine.runAndWait()
-
-
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
@@ -60,3 +38,19 @@ def get_response(text):
     reply = tokenizer.decode(output[0], skip_special_tokens=True)
     # ユーザーの入力ごと含まれるので削除
     return reply.replace(text, "").strip()
+
+
+##voice_output
+engine = pyttsx3.init()
+
+## 日本語音声があったらそれを選択
+for voice in engine.getProperty('voices'):
+	if "Japanese" in voice.name or "Haruka" in voice.name:
+		engine.setProperty('voice',voice.id)
+		break
+
+##engine.say("こんにちは、私はオフラインのAIです。")
+engine.say(get_response(text)) ##responseの関数で返答
+engine.runAndWait()
+
+
