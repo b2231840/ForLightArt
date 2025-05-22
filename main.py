e3f382cf02a9ab2994659a12638a6096829bcb61
b2231840 <b2231840@photon.chitose.ac.jp>
2025-05-22 09:45:08 +0900

    devの変更をこちらに

--- main.py
+++ main.py
@@ -13,15 +13,31 @@ with sr.Microphone() as source:
         print("認識結果:", text)
     except sr.UnknownValueError:
         print("認識できませんでした")
+        exit(0)
 
 ##responce
-#def get_response(text):
-#    if "こんにちは" in text:
-#        return "こんにちは！今日はどうですか？"
-#    elif "さようなら" in text:
-#        return "またね〜！"
-#    else:
-#        return "ごめん、よくわからないや"
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+# モデルとトークナイザの読み込み
+tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt2-medium")
+model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
+
+def get_response(text):
+    input_ids = tokenizer.encode(text, return_tensors="pt")
+    with torch.no_grad():
+        output = model.generate(
+            input_ids,
+            max_length=300, #生成トークンの最大長
+            do_sample=True, #ランダム性を持つかどうか
+            top_p=0.95, #より自然に
+            temperature=0.8, #ランダム性の度合い
+            pad_token_id=tokenizer.pad_token_id,
+            eos_token_id=tokenizer.eos_token_id
+        )
+    reply = tokenizer.decode(output[0], skip_special_tokens=True)
+    # ユーザーの入力ごと含まれるので削除
+    return reply.replace(text, "").strip()
 
 
 ##voice_output
@@ -34,29 +50,9 @@ for voice in engine.getProperty('voices'):
 		break
 
 ##engine.say("こんにちは、私はオフラインのAIです。")
-engine.say(get_response(text)) ##responseの関数で返答
+res = get_response(text)
+engine.say(res) ##responseの関数で返答
+print("返答結果：",res)
 engine.runAndWait()
 
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-
-# モデルとトークナイザの読み込み
-tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt-1b")
-model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt-1b")
-
-def get_response(text):
-    input_ids = tokenizer.encode(text, return_tensors="pt")
-    with torch.no_grad():
-        output = model.generate(
-            input_ids,
-            max_length=100,#生成トークンの最大長
-            do_sample=True,#ランダム性を持つかどうか
-            top_p=0.95,#より自然に
-            temperature=0.8,#ランダム性の度合い
-            pad_token_id=tokenizer.pad_token_id,
-            eos_token_id=tokenizer.eos_token_id
-        )
-    reply = tokenizer.decode(output[0], skip_special_tokens=True)
-    # ユーザーの入力ごと含まれるので削除
-    return reply.replace(text, "").strip()
