23c6b142b611952a817466da2993fdc31cb0c5a2
b2231840 <b2231840@photon.chitose.ac.jp>
2025-05-22 10:11:53 +0900

    resのrinnaを軽量版に変更・mainにresをコピー

--- res.py
+++ res.py
@@ -2,18 +2,18 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 # モデルとトークナイザの読み込み
-tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt-1b")
-model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt-1b")
+tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt2-medium")
+model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
 
 def get_response(text):
     input_ids = tokenizer.encode(text, return_tensors="pt")
     with torch.no_grad():
         output = model.generate(
             input_ids,
-            max_length=100,#生成トークンの最大長
-            do_sample=True,#ランダム性を持つかどうか
-            top_p=0.95,#より自然に
-            temperature=0.8,#ランダム性の度合い
+            max_length=300, #生成トークンの最大長
+            do_sample=True, #ランダム性を持つかどうか
+            top_p=0.95, #より自然に
+            temperature=0.8, #ランダム性の度合い
             pad_token_id=tokenizer.pad_token_id,
             eos_token_id=tokenizer.eos_token_id
         )
